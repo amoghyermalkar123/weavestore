1d7854440fd8892427d274ac6e43579842d925a3
amogh yermalkar <amogh.yermalkar@sugarboxnetworks.com>
2023-06-07 14:54:37 +0530

    Chore: typo fixes

--- messages/success.go
+++ messages/success.go
@@ -1,10 +1,10 @@
 package messages
 
-func Resp(val any, op string, respCode int) map[string]interface{} {
-	var Success = map[string]any{
+func Resp(val any, op string, respCode int) map[string]any {
+	var payload = map[string]any{
 		"Data":   val,
 		"Op":     op,
 		"Status": respCode,
 	}
-	return Success
+	return payload
 }
